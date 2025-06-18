511d748b5a83d3128734e42fd9e3f0321a19e374
DavidDems <daviddemers92@gmail.com>
2025-06-18 13:28:15 -0400

    Set username to email before @ prefix

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -3,16 +3,7 @@ const User = require('../models/User');
 // Create User (only for postman testing, not integrated)
 exports.createUser = async (req, res) => {
   try {
-    const { auth0Id, email, username } = req.body;
-    
-    // Simple validation
-    if (!username.trim() || !email.trim() || !auth0Id) {
-      return res.status(400).json({ error: 'Missing required fields' });
-    }
-
-    if (username.trim().length() < 5 || username.trim().length() > 20) {
-      return res.status(400).json({ error: 'Username must be between 5 and 20 characters.' });
-    }
+    const { auth0Id, email } = req.body;
 
     if (email.trim().length() < 4 || !email.includes("@") || email.trim().length() > 60) {
       return res.status(400).json({ error: 'Email must contain an @' });
@@ -22,7 +13,7 @@ exports.createUser = async (req, res) => {
     const newUser = new User({
       auth0Id,
       email,
-      name: username,
+      name: email.split('@')[0],
       savedBooks: []
     });
 
@@ -47,7 +38,7 @@ exports.getUsers = async (req, res) => {
 // Not validating data synce Auth0 sanitizes the fields
 exports.syncUser = async (req, res) => {
   try {
-    const { auth0Id, email, name } = req.body;
+    const { auth0Id, email } = req.body;
 
     // First check if user exists
     let user = await User.findOne({ auth0Id });
@@ -58,7 +49,7 @@ exports.syncUser = async (req, res) => {
       user = new User({
         auth0Id,
         email,
-        name: name || email.split('@')[0],
+        name: email.split('@')[0],
         savedBooks: []
       });
       await user.save();
